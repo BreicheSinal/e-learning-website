4da45843df1168afa6004f25699cd8e84a6f8781
BreicheSinal <sinalbreiche@gmail.com>
2024-11-18 23:47:55 +0200

    Validated recieved data from frontend

--- src/php/signup.php
+++ src/php/signup.php
@@ -8,3 +8,11 @@ $username = $data->username;
 $email = $data->email;
 $password = password_hash($data->password, PASSWORD_DEFAULT);
 $role_id = $data->role_id; 
+
+// validating data fields sent from frontend
+if (!$username || !$email || !$password || !$role_id) {
+    echo json_encode([
+        'status' => 'error', 
+        'message' => 'PROVIDE ALL FIELDS MATE!']);
+    exit();
+}
