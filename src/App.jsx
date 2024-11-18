a6b61a9f223eabc796d1fe8bcb86145544b377b4
BreicheSinal <sinalbreiche@gmail.com>
2024-11-18 23:25:18 +0200

    Setup commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  console.log("App rerendered");
+
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+};
+
+export default App;
