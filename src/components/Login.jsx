c0ab7abe6627a0dbc849be92f5596a4e29ea0539
BreicheSinal <sinalbreiche@gmail.com>
2024-11-19 23:09:20 +0200

    Added a login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import Form from "../components/Signup";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import "../styles/base.css";
+import "../styles/utilities.css";
+import "../styles/signup.css";
+
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
