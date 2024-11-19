60b13ff2da58f885b517306f4686b37d5f7dfefa
BreicheSinal <sinalbreiche@gmail.com>
2024-11-19 23:24:30 +0200

    Setup UI of login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,40 @@ import "../styles/utilities.css";
 import "../styles/signup.css";
 
 const Login = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="flex column gap center">
+      <h2 className="text-align bold">LOG IN</h2>
+      <div className="flex column gap">
+        <label className="bold">Email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          required
+        />
+      </div>
+      <div className="flex column gap">
+        <label className="bold">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+      </div>
+      <button type="submit" className="button align-self">
+        LOGIN
+      </button>
+    </div>
+  );
 };
 
 export default Login;
