47878c7dc77cffa9a9a691cc5ea338efe700b675
BreicheSinal <sinalbreiche@gmail.com>
2024-11-20 04:11:11 +0200

    Added headers to connection php file

--- src/php/connection.php
+++ src/php/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 header("Access-Control-Allow-Origin: *"); 
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header("Content-Type: application/json");
 
 
