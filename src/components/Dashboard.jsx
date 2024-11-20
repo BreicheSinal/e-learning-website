2841d6c0007be32dc21f1c17a0fa266b16391843
BreicheSinal <sinalbreiche@gmail.com>
2024-11-20 03:50:15 +0200

    Added skeleton of dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import profileLogo from "../assets/icons/person.png";
+import logoutLogo from "../assets/icons/logout.png";
 
 const Dashboard = () => {
-  return <div>HIIIIIIII</div>;
+  return (
+    <div className="dashboard">
+      <div className="navbar flex row gap">
+        <h2>Welcome</h2>
+        <button>
+          <img src={profileLogo} alt="profile icon" />
+        </button>
+        <button>
+          <img src={logoutLogo} alt="log out icon" />
+        </button>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
