23cbf5fa0bad5132ff26a5c24e4b0b9f669dddb3
BreicheSinal <sinalbreiche@gmail.com>
2024-11-20 12:50:57 +0200

    Handled GET req to fetch all courses

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,6 +1,19 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { requestApi } from "../utils/request";
+import { requestMethods } from "../utils/enums/requestMethods";
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  const fetchCourses = async () => {
+    try {
+      const result = await requestApi({
+        method: requestMethods.GET,
+        route: "",
+      });
+      setCourses(result.courses);
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
+  };
   return <div></div>;
 };
 
