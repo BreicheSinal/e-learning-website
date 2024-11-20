61f6eb98bfb0dc3a4963db6bef8e0adba1b45c64
BreicheSinal <sinalbreiche@gmail.com>
2024-11-20 03:55:58 +0200

    Added some styles dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import profileLogo from "../assets/icons/person.png";
 import logoutLogo from "../assets/icons/logout.png";
 
+import "../styles/dashboard.css";
+
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div className="navbar flex row gap">
+      <div className="navbar flex row gap align-center space-between">
         <h2>Welcome</h2>
-        <button>
-          <img src={profileLogo} alt="profile icon" />
-        </button>
-        <button>
-          <img src={logoutLogo} alt="log out icon" />
-        </button>
+        <div className="flex gap">
+          <button>
+            <img src={profileLogo} alt="profile icon" className="icon" />
+          </button>
+          <button>
+            <img src={logoutLogo} alt="log out icon" className="icon" />
+          </button>
+        </div>
       </div>
       <div className="main"></div>
     </div>
