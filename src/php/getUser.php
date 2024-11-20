53c7b38887c52ec71aec837f3a73530130cfac87
BreicheSinal <sinalbreiche@gmail.com>
2024-11-20 03:59:43 +0200

    Created get user api | Fetched headers

---
+++ src/php/getUser.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'connection.php';
+require "vendor/autoload.php";
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$secretKey = "#HALA_MADRID";
+
+// fetching headers
+$headers = getallheaders();
+if (!isset($headers['Authorization'])) {
+    http_response_code(400);
+    echo json_encode(['message' => 'Authorization header missing']);
+    exit;
+}
